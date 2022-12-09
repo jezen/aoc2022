b78f91b974d77c0b629eb3eb8c265fefe92753ac
Jezen Thomas <jezen@jezenthomas.com>
2022-12-09 11:33:39 +0100

    Improve 06

--- 06/Main.hs
+++ 06/Main.hs
@@ -9,22 +9,16 @@ import Conduit
 import Data.Conduit.Combinators qualified as C
 import Data.Set qualified as Set
 
-getChars :: IO String
-getChars = runConduitRes $ sourceFile "input"
-  .| C.decodeUtf8
-  .| C.foldlE (\cs c -> cs <> [c]) []
-
 isUniq :: [Char] -> Bool
 isUniq cs = sort cs /= sort (Set.toList (Set.fromList cs))
 
-f :: Int -> [Char] -> IO Int
-f n xs = runConduit $ yieldMany xs
+f :: Int -> IO Int
+f n = runConduitRes $ sourceFile "input"
+  .| C.decodeUtf8
+  .| C.concat
   .| C.slidingWindow n
   .| C.takeWhile isUniq
   .| C.length <&> (n +)
 
 main :: IO ()
-main = do
-  chars <- getChars
-  f 4 chars  >>= print
-  f 14 chars >>= print
+main = (,) <$> f 4 <*> f 14 >>= print
